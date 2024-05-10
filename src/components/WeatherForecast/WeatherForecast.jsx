a6f45db5da755f01f45842d759d25eb8b45f19e9
Yelnats <s.jeanjr@gmail.com>
2024-05-10 14:19:18 -0400

    Bug Search  in code

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import './WeatherForecast.css';
+
+const Forecast = (props) => {
+const {weatherForecast} = props;
+
+    return (
+        <div className="weather">
+            <h2>{props.day}</h2>
+            <img src={props.img} alt={props.imgAlt} />
+            <p><span>conditions: </span>{props.conditions}</p>
+            <p><span>time: </span>{props.time}</p>
+        </div>
+    );
+};
+
+export default Forecast;
